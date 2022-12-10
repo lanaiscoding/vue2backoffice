fff3a461d9e835513a6de79d264af5f900368f3e
yr <ti_la@qq.com>
2022-12-10 22:50:44 +0800

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template lang="">
+    <div>
+        <h1>Home</h1>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+}
+</script>
+<style lang="">
+    
+</style>
---
+++ src/views/Main.vue
@@ -0,0 +1,29 @@
+<template lang="">
+    <el-container>
+         <el-aside width="200px">
+            <CommonAside />
+         </el-aside>
+         <el-container>
+            <el-header>Header</el-header>
+            <el-main>
+             <router-view></router-view>
+            </el-main>
+        </el-container>
+    </el-container>
+</template>
+<script>
+import CommonAside from '../components/CommonAside.vue'
+export default {
+    data() {
+        return {
+
+        }
+    },
+    components: {
+        CommonAside
+    }
+}
+</script>
+<style lang="">
+    
+</style>
